9fafc5e5ed438150f0d555ef9558c02642c9c073
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-12 21:39:47 +0900

    Load diagram grammar and speaker notes layer

--- site/app.js
+++ site/app.js
@@ -14,7 +14,7 @@
     document.head.append(link);
   });
 
-  ['./v2.css', './v3.css', './v4.css'].forEach((href) => {
+  ['./v2.css', './v3.css', './v4.css', './v5.css'].forEach((href) => {
     const layerStyle = document.createElement('link');
     layerStyle.rel = 'stylesheet';
     layerStyle.href = href;
@@ -84,6 +84,7 @@
     .then(() => loadScript('./v2-fixes.js'))
     .then(() => loadScript('./v3.js'))
     .then(() => loadScript('./v4.js'))
+    .then(() => loadScript('./v5.js'))
     .catch(() => {
       // Base content remains usable even if an enhancement layer fails.
     });
