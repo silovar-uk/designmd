90b7de9007b9ab52eedfae0acf28f475432994c7
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-12 22:16:35 +0900

    Load audience-first and plain slide layer

--- site/app.js
+++ site/app.js
@@ -14,7 +14,7 @@
     document.head.append(link);
   });
 
-  ['./v2.css', './v3.css', './v4.css', './v5.css'].forEach((href) => {
+  ['./v2.css', './v3.css', './v4.css', './v5.css', './v6.css'].forEach((href) => {
     const layerStyle = document.createElement('link');
     layerStyle.rel = 'stylesheet';
     layerStyle.href = href;
@@ -85,6 +85,7 @@
     .then(() => loadScript('./v3.js'))
     .then(() => loadScript('./v4.js'))
     .then(() => loadScript('./v5.js'))
+    .then(() => loadScript('./v6.js'))
     .catch(() => {
       // Base content remains usable even if an enhancement layer fails.
     });
